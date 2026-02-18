c61e0de82be05ddde8a574f6731e810b4d6a669a
Julia Nguyen <julianguyenpt@gmail.com>
2026-02-17 21:51:23 -0500

    feat: remove bad probes

--- workflow/scripts/process_methylation.R
+++ workflow/scripts/process_methylation.R
@@ -22,8 +22,9 @@ meta <- read.csv("metadata/meta_data_research.csv")
 
 # TODO: FIND MAPPING OF METADATA IDS AND IDAT IDS
 
-# load in CpG annotation
-ann <- getAnnotation(IlluminaHumanMethylationEPICanno.ilm10b2.hg19)
+# load in CpG annotation for both arrays
+annEPICv1 <- getAnnotation(IlluminaHumanMethylationEPICanno.ilm10b4.hg19)
+annEPICv2 <- getAnnotation(IlluminaHumanMethylationEPICv2anno.20a1.hg38)
 
 ###########################################################
 # Make IDAT meta
@@ -51,7 +52,7 @@ targets$Array <- ifelse(
 )
 
 ###########################################################
-# Get RGsets
+# Get RGSets
 ###########################################################
 
 # IlluminaHumanMethylationEPIC (3 samples)
@@ -107,3 +108,53 @@ compare_norm <- function(RGSet, label) {
 
 compare_norm(rgSet_EPICv1, label = "EPICv1")
 compare_norm(rgSet_EPICv2, label = "EPICv2")
+
+###########################################################
+# Downstream QC after selecting normalization method
+# below using preprocessIllumina()
+###########################################################
+
+# remove 
+QCv1 <- getQC(preprocessRaw(rgSet_EPICv1))
+QCv2 <- getQC(preprocessRaw(rgSet_EPICv2))
+
+rgSet_EPICv1 <- rgSet_EPICv1[,
+    !sampleNames(rgSet_EPICv1) %in% rownames(QCv1)[QCv1$mMed + QCv1$uMed < 21]
+]
+rgSet_EPICv2 <- rgSet_EPICv2[,
+    !sampleNames(rgSet_EPICv2) %in% rownames(QCv2)[QCv2$mMed + QCv2$uMed < 21]
+]
+
+# normalize with preprocessIllumina()
+MSetv1 <- preprocessIllumina(rgSet_EPICv1)
+MSetv2 <- preprocessIllumina(rgSet_EPICv2)
+
+# convert to GenomicRatioSet (remove if change to Quantile normalization)
+GRSetv1 <- ratioConvert(mapToGenome(MSetv1))
+GRSetv2 <- ratioConvert(mapToGenome(MSetv2))
+
+###########################################################
+# Remove bad probes
+###########################################################
+
+# helper function to get cross reactive and sex chr probes
+bad_probes <- function(ann) {
+    return(c(
+        # cross reactive probes
+        rownames(ann)[
+            !is.na(ann$Probe_rs)
+            # can optionally keep these?
+            | !is.na(ann$CpG_rs)
+            | !is.na(ann$SBE_rs)
+        ],
+        # probes on sex chrs
+        rownames(ann)[ann$chr %in% c("chrX", "chrY")]
+    ))
+}
+
+GRSetv1 <- GRSetv1[!rownames(GRSetv1) %in% bad_probes(annEPICv1),]
+GRSetv2 <- GRSetv2[!rownames(GRSetv2) %in% bad_probes(annEPICv2),]
+
+# remove probes with SNPs (can optionally drop by maf)
+GRSetv1 <- dropLociWithSnps(GRSetv1)
+GRSetv2 <- dropLociWithSnps(GRSetv2)
